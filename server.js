291a60bf8dc3a82b9a54400089ac8427679e0377
lihuayi <revordema@qq.com>
2024-04-10 23:22:08 +0800

    feature: add init file

--- server.js
+++ server.js
@@ -155,6 +155,7 @@ var server = app.listen(80, function () {
  
   var host = server.address().address
   var port = server.address().port
- 
+
+
   console.log("应用实例，访问地址为 http://%s:%s", host, port)
 })
